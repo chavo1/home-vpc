554154b14d9df8d0e7a5387d95d5821f11143bae
chavo1 <chavdar.rakov@gmail.com>
2019-11-25 14:20:51 +0200

    output

--- outputs_vpc.tf
+++ outputs_vpc.tf
@@ -10,3 +10,6 @@ output "private_subnet_cidr" {
 output "main_route_table_id" {
   value = aws_vpc.consul_vpc.main_route_table_id
 }
+output "security_group_id" {
+  value = aws_security_group.sg-group.id
+}
